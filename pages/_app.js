f86f71ce4f3619075b23f55b29c9871fb59c2e8d
Reventon1 <dima.crsot@yandex.ru>
2022-02-18 20:48:28 +0300

    konstrukt sajta

--- pages/_app.js
+++ pages/_app.js
@@ -3,3 +3,5 @@ import '../styles/globals.scss'
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+export default MyApp 
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 export default function Home() {
-  return(
-    <div>Главная страница</div>
-  )
+  return (
+    <div className="site">
+      <div className="site_wrapper">
+        <div className="site_menu">Меню</div>
+        <div className="site_content">
+          <div className="content_slider">Слайдер</div>
+        </div>
+      </div>
+    </div>
+  );
 }
