5159d2926cf5de10e972c8135c74ab86da79c657
Шмырин Дмитрий Сергеевич <pl8431@C201N3.pl1.edu>
2022-02-19 15:48:42 +0500

    bubububub

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,12 @@ export default function Home() {
       <div className="site_wrapper">
         <div className="site_menu">Меню</div>
         <div className="site_content">
-          <div className="content_slider">Слайдер</div>
+          <div className="content_slider">
+          <div className="imagtikva"></div>
+          <div className="content_slider_text">
+          <div className="content_slider_text_text">UOOOO TRICK OR TREAT!!</div>
+          </div>
+          </div>
         </div>
       </div>
     </div>
